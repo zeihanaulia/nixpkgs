9b57be406c9f015ce9ea8ef24c16a3efadde0ba2
zeihanaulia <zeihan.aulia@outlook.com>
2022-06-30 18:39:43 +0000

    add golang 1.18

--- flake.nix
+++ flake.nix
@@ -1,5 +1,33 @@
 {
   inputs = {
     home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
   };
+
+  outputs = {self, home-manager, nixpkgs, ...}@inputs:
+      let 
+	system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system}; 
+      in 
+	{
+             homeConfigurations = {
+                 zeihanaulia = home-manager.lib.homeManagerConfiguration {
+                      inherit pkgs;
+		      modules = [
+		          ({pkgs, ...}:{
+                              home.stateVersion = "22.05";
+                              home.username = "zeihanaulia";
+			      home.homeDirectory = "/home/zeihanaulia/";
+
+			      home.packages = with pkgs;[
+                                  vim
+                              ];
+
+                              programs.go.enable = true;
+			      programs.go.package = pkgs.go_1_18;
+                          })	
+		      ];
+                 };
+             };
+        };
 }

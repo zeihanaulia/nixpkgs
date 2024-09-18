e6abeb37e33681dfd870c4842ea6fc04ae2fc133
Zeihan Aulia <zeihanaulia@gmail.com>
2024-09-18 15:02:26 +0700

    add pnpm

--- flake.nix
+++ flake.nix
@@ -51,6 +51,7 @@
                   gopls           # Go language server protocol package
                   nodejs          # Latest Node.js package available in Nixpkgs
                   yarn            # Latest yarn package available in Nixpkgs
+                  pnpm
                   python3         # Latest Python 3 package available in Nixpkgs
                   python311Packages.pip # Python pip package manager
                   rustup          # Rustup installer from Nixpkgs
